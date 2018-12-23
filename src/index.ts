1bc7a46ba3692070f47aa62b54388714a350faf6
Vladislav Korbut <vkorbut@yandex-team.ru>
2018-12-23 13:50:37 +0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import {BulizhnickApiApplication} from './application';
 import {ApplicationConfig} from '@loopback/core';
+import * as fs from 'fs';
 
 export {BulizhnickApiApplication};
 
 export async function main(options: ApplicationConfig = {}) {
+  options = {
+    rest: {
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+      maxAge: 86400,
+      credentials: true,
+    },
+  };
+
   const app = new BulizhnickApiApplication(options);
   await app.boot();
   await app.start();
